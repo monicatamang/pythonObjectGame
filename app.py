70d2ef537db1a839d8a55d444bf5b8fe0ce733bb
Monica <tamang.monica@gmail.com>
2021-05-31 14:35:34 -0600

    Changed the structure of the gameboard, working on adding coins

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ print("To move up: w")
 print("To move down: s")
 print("To move left: a")
 print("To move right: d")
+print("Coins: C")
+# print("Enemies: E")
 
 print("Try to get to the end! Good Luck!")
 print("-----------------------------")
@@ -17,11 +19,12 @@ board = gameboard.GameBoard()
 
 # Create a new Player called played starting at position 3,2
 # played = player.Player(3,2)
-played = player.Player(20,18)
+played = player.Player(20,2)
 
 while True:
     # board.printBoard(player.rowPosition, player.columnPosition)
     board.printBoard(played.rowPosition, played.columnPosition)
+    board.trackCoins(played.rowPosition, played.columnPosition)
     selection = input("Make a move: ")
 
     # TODO
@@ -59,4 +62,5 @@ while True:
     check_player_win = board.checkWin(played.rowPosition, played.columnPosition)
     if(check_player_win == True):
         print("Congratulations! You have won the game!")
+        print(f"Total Coins Collected: {board.coinsCollected}")
         break
